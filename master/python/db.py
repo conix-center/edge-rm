9704836fd7759038634e67076aabbfdaa1251aa6
Joshua Adkins <adkins@berkeley.edu>
2020-05-22 15:16:21 -0700

    add support for scalars, sets, text

--- master/python/db.py
+++ master/python/db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import messages_pb2
 
 dbname = 'edgerm.db'
 
@@ -9,23 +10,31 @@ def refresh_db():
 		c.execute("DROP TABLE IF EXISTS resources")
 		c.execute("DROP TABLE IF EXISTS attributes")
 		c.execute("CREATE TABLE agents (id integer primary key autoincrement, conn text)")
-		c.execute("CREATE TABLE resources (agentID integer, name text, type integer, value blob)")
-		c.execute("CREATE TABLE attributes (agentID integer, name text, type integer, value blob)")
+		c.execute("CREATE TABLE resources (agentID integer, name text, type integer, scalar_value double, set_value text, text_value text)")
+		c.execute("CREATE TABLE attributes (agentID integer, name text, type integer, scalar_value double, set_value text, text_value text)")
 		conn.commit()
 
 def add_agent(resources, attributes, connection):
-	print(resources)
-	print(attributes)
 	with sqlite3.connect(dbname) as conn:
 		c = conn.cursor()
 		c.execute("INSERT INTO agents (conn) VALUES (?)", (connection,))
 		agent_id = c.lastrowid
 		for (rname, rtype, rval) in resources:
-			print("Inserting resource",rname,rtype,rval)
-			c.execute("INSERT INTO resources VALUES (?,?,?,?)", (agent_id, rname, rtype, rval))
+			if rtype == messages_pb2.Value.SCALAR:
+				c.execute("INSERT INTO resources VALUES (?,?,?,?,?,?)", (agent_id, rname, rtype, rval, None, None))
+			elif rtype == messages_pb2.Value.SET:
+				for r in rval:
+					c.execute("INSERT INTO resources VALUES (?,?,?,?,?,?)", (agent_id, rname, rtype, None, r, None))
+			elif rtype == messages_pb2.Value.TEXT:
+				c.execute("INSERT INTO resources VALUES (?,?,?,?,?,?)", (agent_id, rname, rtype, None, None, rval))
 		for (rname, rtype, rval) in attributes:
-			print("Inserting attribute",rname,rtype,rval)
-			c.execute("INSERT INTO attributes VALUES (?,?,?,?)", (agent_id, rname, rtype, rval))
+			if rtype == messages_pb2.Value.SCALAR:
+				c.execute("INSERT INTO resources VALUES (?,?,?,?,?,?)", (agent_id, rname, rtype, rval, None, None))
+			elif rtype == messages_pb2.Value.SET:
+				for r in rval:
+					c.execute("INSERT INTO resources VALUES (?,?,?,?,?,?)", (agent_id, rname, rtype, None, r, None))
+			elif rtype == messages_pb2.Value.TEXT:
+				c.execute("INSERT INTO resources VALUES (?,?,?,?,?,?)", (agent_id, rname, rtype, None, None, rval))
 		conn.commit()
 		return agent_id
 	return None
@@ -45,15 +54,19 @@ def get_all():
 			for rrow in rrows:
 				rname = rrow[1]
 				rtype = rrow[2]
-				rval = rrow[3]
-				resources.append((rname, rtype, rval))
+				scalar_val = rrow[3]
+				set_val = rrow[4]
+				text_val = rrow[5]
+				resources.append((rname, rtype, scalar_val, set_val, text_val))
 			c.execute("SELECT * FROM attributes WHERE agentID = ?",str(agent_id))
 			rrows = c.fetchall()
 			for rrow in rrows:
 				rname = rrow[1]
 				rtype = rrow[2]
-				rval = rrow[3]
-				attributes.append((rname, rtype, rval))
+				scalar_val = rrow[3]
+				set_val = rrow[4]
+				text_val = rrow[5]
+				attributes.append((rname, rtype, scalar_val, set_val, text_val))
 			agents[agent_id] = (resources, attributes)
 		conn.commit()
 	return agents
