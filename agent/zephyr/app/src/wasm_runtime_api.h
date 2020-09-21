026632b8899bdb8dd040b26b20e958bba2f0842a
x0oo0x <richard97@g.ucla.edu>
2020-09-21 12:21:19 -0700

    Update wasm_runtime_api.h

--- agent/zephyr/app/src/wasm_runtime_api.h
+++ agent/zephyr/app/src/wasm_runtime_api.h
@@ -3,8 +3,14 @@
 
 #include "wasm_export.h"
 
-void mqttsnConnect(wasm_exec_env_t exec_env);
-int get_time(void);
-int convert(int cycles);
+void waMQTTSNConnect(wasm_exec_env_t exec_env, char* clientID, int keepAlive, char* gateAddr, int port);
+void waMQTTSNDisconnect(wasm_exec_env_t exec_env);
+void waMQTTSNStart(wasm_exec_env_t exec_env, int port);
+void waMQTTSNStop(wasm_exec_env_t exec_env);
+void waMQTTSNPub(wasm_exec_env_t exec_env, char *data, int qos, char *topicName);
+void waMQTTSNSub(wasm_exec_env_t exec_env, int qos, char *topicName);
+int waMQTTSNReg(wasm_exec_env_t exec_env, char *topicName);
+int waGetCPUCycles(wasm_exec_env_t exec_env);
+int waConvertCyclesToMilis(wasm_exec_env_t exec_env ,int cycles);
 
 #endif
