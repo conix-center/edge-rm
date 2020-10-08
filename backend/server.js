a32a38c50b121149d99e2fd41e6557b5a9f1701d
Joseph Noor <josephgnoor@gmail.com>
2020-10-07 18:44:18 -0700

    got initial d3 visualization

--- backend/server.js
+++ backend/server.js
@@ -71,6 +71,73 @@ app.get('/framework', function(req, res) {
 	res.status(200).sendFile(path.join(__dirname + '/tasks.json'));
 })
 
+app.get('/network.json', function(req, res) {
+	request('http://128.97.92.77/', function(error, response, body) {
+		var result = {'nodes':[],'links':[]}
+		if (error) {
+			log.info(error);
+			return res.send(result);
+		}
+		var agents = JSON.parse(body);
+		//for every agent
+		for (var i = 0; i < agents.length; i++) {
+			//add links to every other agent
+			for (var j = 0; j < result['nodes'].length; j++) {
+				result['links'].push({
+					'source':result['nodes'][j].id,
+					'target':agents[i].id,
+					'value':1
+				})
+			}
+			//add the agent to the list of nodes
+			result['nodes'].push({
+				id: agents[i].id,
+				name: agents[i].name,
+				group: 1
+			})
+		}
+		//add the master, framework, and client
+		var master = {
+			id: 'Master',
+			name: "Resource Manager Master",
+			group:2
+		}
+		var framework = {
+			id: 'Framework',
+			name: 'Test Framework',
+			group:3
+		}
+		var client = {
+			id: 'Me',
+			name: 'Me',
+			group:4
+		}
+		//connect all agents to the master
+		for(var i = 0; i < result['nodes'].length; i++) {
+			result['links'].push({
+				'source':result['nodes'][i].id,
+				'target':master.id,
+				'value':10
+			})
+		}
+		result['nodes'].push(master)
+		result['nodes'].push(framework)
+		result['nodes'].push(client)
+		//connect framework to master
+		result['links'].push({
+			source:framework.id,
+			target:master.id,
+			value:3
+		})
+		result['links'].push({
+			source:client.id,
+			target:framework.id,
+			value:4
+		})
+		res.send(result);
+	});
+});
+
 app.get('/agents', function(req, res) {
 	log.info("GET /agents")
 	request('http://128.97.92.77/').on('error', (e) => {res.status(500).send({})}).pipe(res);
