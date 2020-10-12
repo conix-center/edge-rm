064da15be4641d461302ea9a626a53ed82a4511e
Joshua Adkins <adkins@berkeley.edu>
2020-10-12 13:17:03 -0700

    update coap file server to return json with time and value

--- docker/coapserver/client.js
+++ docker/coapserver/client.js
@@ -3,31 +3,37 @@ var coap        = require('coap');
 var req = coap.request({
   host: 'localhost',
   // host: 'conixdb.com',
-  // port: 3002,
+  port: 3002,
   pathname: '/test',
   method: "POST"
 })
 // var req = coap.request('coap://conixdb.com:3002/post')
 
 req.on('response', function(res) {
-  res.pipe(process.stdout)
+  //res.pipe(process.stdout)
   res.on('end', function() {
     // process.exit(0)
   })
 })
 
-req.write('TESTING2')
-
+req.write('1')
 req.end()
 
 setTimeout(function() {
   // var req2 = coap.request('coap://conixdb.com:3002/latest')
-  var req2 = coap.request('coap://localhost/test')
+  var req2 = coap.request({
+    host: 'localhost',
+    port: 3002,
+    pathname: '/test',
+    method: "GET"
+  });
+
   req2.on('response', function(res) {
-    res.pipe(process.stdout)
+    console.log(JSON.parse(String(res.payload)));
     res.on('end', function() {
       process.exit(0)
     })
   })
+
   req2.end()
 }, 2000)
--- docker/coapserver/server.js
+++ docker/coapserver/server.js
@@ -10,6 +10,8 @@ server.on('request', function(req, res) {
   var pathName = req.url.split('/')[1];
   var pathToFile = path.join(__dirname + "/files/" + pathName);
   console.log("HERE", pathName)
+  console.log(req.method);
+
   if(pathName == '') {
     console.log("GET /")
     return res.end("Hello World!")
@@ -19,18 +21,38 @@ server.on('request', function(req, res) {
     console.log("GET " + pathName)
     if(fs.existsSync(pathToFile)) {
       res.code = 200
+      res.setOption("Content-Format", "application/json");
       fs.readFile(pathToFile, 'utf8', function(err, data) {
-        if(err) res.end(err);
-        else res.end(data);
+        if(err) {
+          console.log("Error reading file");
+          res.end(err);
+          return;
+        }
+
+        var r = [];
+        var lines = data.split('\n')
+
+        for (var i = 0; i < lines.length; i++) {
+          var line = lines[i];
+          if(line.length != 0) {
+            var t = line.split(':')[0];
+            var v = line.split(':')[1];
+            r.push({'time':t,'value':v})
+          }
+        }
+
+        res.end(JSON.stringify(r));
+
       })
     } else {
+      console.log("File doesn't exist");
       res.code = 404;
       res.end('');
     }
   } else if (req.method == "POST") {
     console.log("POST " + pathName)
-    var value = req.payload.toString()
-    fs.writeFile(pathToFile, value, function(err) {
+    var v = Date.now().toString() + ':' + req.payload.toString() + '\n';
+    fs.appendFile(pathToFile, v, function(err) {
       if(err) console.log(err)
       res.end("Got it!")
     })
